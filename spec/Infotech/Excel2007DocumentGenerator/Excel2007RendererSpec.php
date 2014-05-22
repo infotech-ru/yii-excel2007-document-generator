1d6534d5612c78ad8d473a970bbe5e84eb29f2f5
Anton Tyutin <anton@tyutin.ru>
2014-05-22 14:19:53 +0400

    Initial commit. Basic functionality.

---
+++ spec/Infotech/Excel2007DocumentGenerator/Excel2007RendererSpec.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace spec\Infotech\Excel2007DocumentGenerator;
+
+use PHPExcel_IOFactory;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class Excel2007RendererSpec extends ObjectBehavior
+{
+    public function getMatchers()
+    {
+        $createTmpFile = function ($contents) {
+            $tmpfile = tempnam(sys_get_temp_dir(), 'spec_fixture_');
+            file_put_contents($tmpfile, $contents);
+            return $tmpfile;
+        };
+        $extractDocument = function ($contents) use ($createTmpFile) {
+
+            $tmpfile = $createTmpFile($contents);
+
+            $docContents = @file_get_contents('zip://' . $tmpfile . '#xl/workbook.xml');
+            unlink($tmpfile);
+
+            return $docContents;
+        };
+        return [
+            'beXlsXDocument' => function ($subject) use ($extractDocument) {
+                return false !== $extractDocument($subject);
+            },
+            'contains' => function($subject, $strings) use ($createTmpFile) {
+                $tmpfile = $createTmpFile($subject);
+
+                $book = PHPExcel_IOFactory::load($tmpfile);
+                unlink($tmpfile);
+
+                $cellValues = array();
+
+                foreach ($book->getAllSheets() as $sheet) {
+                    foreach ($sheet->getRowIterator() as $row) {
+                        foreach ($row->getCellIterator() as $cell) {
+                            $cellValues[] = $cell->getValue();
+                        }
+                    }
+                }
+
+                preg_match_all(
+                    '/' . implode('|', array_map('preg_quote', (array)$strings)) . '/',
+                    implode("\n", $cellValues),
+                    $matches
+                );
+
+                return !array_diff((array)$strings, $matches[0]);
+            }
+        ];
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Infotech\Excel2007DocumentGenerator\Excel2007Renderer');
+    }
+
+    function it_should_fillup_template_with_values()
+    {
+        $fixtureTemplate = __DIR__ . '/../../fixtures/simple_template.xlsx';
+        $data = [
+            'PLACEHOLDER_1' => 'Replaced placeholder 1',
+            'PLACEHOLDER_2' => 'Replaced placeholder 2',
+            'PLACEHOLDER_3' => '3',
+        ];
+
+        $result = $this->render($fixtureTemplate, $data);
+        $result->shouldBeXlsXDocument();
+        $result->shouldContains($data);
+    }
+
+    function it_should_throw_an_exception_when_template_not_found()
+    {
+        $fixtureTemplate = __DIR__ . '/../../fixtures/unexistent_template.xlsx';
+        $data = [
+            'PLACEHOLDER_1' => 'Replaced placeholder 1',
+        ];
+
+        $this->shouldThrow('CException')->during('render', array($fixtureTemplate, $data));
+    }
+}
---
+++ src/Excel2007Renderer.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * This file is part of the infotech/yii-excel2007-document-generator package.
+ *
+ * (c) Infotech, Ltd
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Infotech\Excel2007DocumentGenerator;
+
+use Infotech\DocumentGenerator\Renderer\RendererInterface;
+use ZipArchive;
+use CException;
+
+class Excel2007Renderer implements RendererInterface
+{
+    /**
+     * Render template with data.
+     *
+     * @param string $templatePath
+     * @param array $data
+     * @throws \CException While template read error or temporary document write error
+     * @return string Rendered document as binary string
+     */
+    public function render($templatePath, array $data)
+    {
+        $tmpPath = sys_get_temp_dir() . '/' . uniqid('excel_render_');
+
+        if (!@copy($templatePath, $tmpPath)) {
+            throw new CException('Error while reading a template file');
+        }
+
+        $zip = new ZipArchive();
+        if (!$zip->open($templatePath) || !$sharedStrings = $zip->getFromName('xl/sharedStrings.xml')) {
+            throw new CException('Unsupported template file format. Required xlsx (Excel2007) file');
+        }
+
+        $replaceFn = function ($matches) use ($data) {
+            return isset($data[$matches[1]]) ? $data[$matches[1]] : $matches[0];
+        };
+
+        $zip->addFromString('xl/sharedStrings.xml', preg_replace_callback('/\$\{(\w+)\}/', $replaceFn, $sharedStrings));
+
+        if (!$zip->close() ) {
+            throw new CException('Error while writing temporary rendered file');
+        }
+
+        return $this->getTemporaryFileContents($tmpPath);
+    }
+
+    /**
+     * @param string $filePath
+     * @return string
+     */
+    private function getTemporaryFileContents($filePath)
+    {
+        $contents = file_get_contents($filePath);
+        unlink($filePath);
+
+        return $contents;
+    }
+}
